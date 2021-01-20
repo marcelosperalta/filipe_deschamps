cff76e9b694bdbc2ce94d9ee1b6c72f2e16eeb6a
marcelosperalta <marcelosperalta@gmail.com>
2021-01-20 20:15:06 +0100

    filipe_deschamps

--- 20210118/pages/api/tempo.js
+++ 20210118/pages/api/tempo.js
@@ -1,4 +1,10 @@
-function tempo(request, response) {
+async function tempo(request, response) {
+
+    // variável de ambiente
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
+
+    await fetch(`https://api.convertkit.com/v3/subscrivers?api_secret=${apiSecret}`);
+    = await subscriversResponse.json();
     const dynamicDate = new Date();
 
     response.json({
