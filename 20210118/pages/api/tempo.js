8266640d6d8c6f33c73577159177e6fa8cce79ed
marcelosperalta <marcelosperalta@gmail.com>
2021-01-21 16:31:08 +0100

    filipe_deschamps

--- 20210118/pages/api/tempo.js
+++ 20210118/pages/api/tempo.js
@@ -1,19 +1,19 @@
 async function tempo(request, response) {
 
-    // variável de ambiente
-    // criar um arquivo ".env" na raiz do projeto e colocar o valor da variável "CONVERTKIT_API_SECRET" nesse arquivo
-    // exemplo: CONVERTKIT_API_SECRET=password
-    const apiSecret = process.env.CONVERTKIT_API_SECRET;
+    // // variável de ambiente
+    // // criar um arquivo ".env" na raiz do projeto e colocar o valor da variável "CONVERTKIT_API_SECRET" nesse arquivo
+    // // exemplo: CONVERTKIT_API_SECRET=password
+    // const apiSecret = process.env.CONVERTKIT_API_SECRET;
 
     const dynamicDate = new Date();
 
-    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscrivers?api_secret=${apiSecret}`);
-    const subscribersResponseJson = await subscribersResponse.json();
-    const inscritos = subscribersResponseJson.total_subscribers;
+    // const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscrivers?api_secret=${apiSecret}`);
+    // const subscribersResponseJson = await subscribersResponse.json();
+    // const inscritos = subscribersResponseJson.total_subscribers;
 
     response.json({
         date: dynamicDate.toGMTString(),
-        inscritos: inscritos
+        // inscritos: inscritos
     })
 }
 
